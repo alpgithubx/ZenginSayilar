d218f51783398a16a2417dd0250dd3e141653226
Alp Tuna Şahin <118995494+alpgithubx@users.noreply.github.com>
2023-06-29 16:15:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenginSayılar {
+    internal class Program {
+        static void Main(string[] args) {
+            for (int i = 1; i < 1000000; i++) {
+                int sum = 0;
+                for (int j = 1; j < i; j++) {
+                    if (i % j == 0) {
+                        sum += j;
+                    }
+                }
+                if (sum > i) {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
